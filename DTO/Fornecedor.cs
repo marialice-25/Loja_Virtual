38a22d187d092ccc217f17249d7c34791b8d0ef5
Maria Alice <78799728+marialice-25@users.noreply.github.com>
2021-04-24 13:42:06 -0300

    Add files via upload

---
+++ DTO/Fornecedor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loja_Virtual_Dev.DTO
+{
+    public class Fornecedor
+    {
+        private int id, produtosid;
+        private string nome, cnpj, email, telefone, nomeRepresentante, telefoneRepresentante;
+
+        public string TelefoneRepresentante { get => telefoneRepresentante; set => telefoneRepresentante = value; }
+        public string NomeRepresentante { get => nomeRepresentante; set => nomeRepresentante = value; }
+        public int Produtosid { get => produtosid; set => produtosid = value; }
+        public int Id { get => id; set => id = value; }
+
+        public string Telefone
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.telefone = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de telefone obrigatório");
+                }
+            }
+            get { return this.telefone; }
+        }
+
+        public string Nome
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.nome = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de nome é obrigatório");
+                }
+            }
+            get { return this.nome; }
+        }
+
+        public string Cnpj
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.cnpj = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de cnpj é obrigatório");
+                }
+            }
+            get { return this.cnpj; }
+        }
+        public string Email
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.email = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de email obrigatório");
+                }
+            }
+            get { return this.email; }
+        }
+    }
+}
---
+++ DTO/Produto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loja_Virtual_Dev.DTO
+{
+    public class Produto
+    {
+        private int id, quantidade, categoriaID, fornecedorID;
+        private string nome, descricao, imagem, validade;
+        private double valor;
+
+        public int Id { get => id; set => id = value; }
+        public int FornecedorID { get => fornecedorID; set => fornecedorID = value; }
+        public double Valor { get => valor; set => valor = value; }
+        public string Imagem { get => imagem; set => imagem = value; }
+
+        public string Validade { get => validade; set => validade = value; }
+
+        public string Nome
+        {
+            set
+            {
+                if(value != string.Empty)
+                {
+                    this.nome = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de produto é obrigatório");
+                }
+            }
+            get { return this.nome; }
+        }
+
+        public string Descricao
+        {
+            set
+            {
+                if(value != string.Empty)
+                {
+                    this.descricao = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de descrição é obrigatório");
+                }
+            }
+            get { return this.descricao; }
+        }
+
+        public int Quantidade
+        {
+            set
+            {
+                if(value != 0)
+                {
+                    this.quantidade = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de quantidade é obrigatório");
+                }
+            }
+            get { return this.quantidade; }
+        }
+
+        public int CategoriaID
+        {
+            set
+            {
+                if (value != 0)
+                {
+                    this.categoriaID = value;
+                }
+                else
+                {
+                    throw new Exception("Campo de categoria é obrigatório");
+                }
+            }
+            get { return this.categoriaID; }
+        }
+    }
+}
